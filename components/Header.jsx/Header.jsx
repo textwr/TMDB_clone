a4a8fc6a1723bd07ae5e0c8750108904ce47a415
hitto <116558066+textwr@users.noreply.github.com>
2024-09-27 17:40:44 +0900

    .

---
+++ components/Header.jsx/Header.jsx
@@ -0,0 +1,52 @@
+import styles from "./styles.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.header_left}>
+          <a href="">
+            <img src="../assets/logo.svg" alt="" width={154} height={20} />
+          </a>
+          <ul className={styles.header_leftUL}>
+            <li>
+              <a href="#">메뉴1</a>
+            </li>
+            <li>
+              <a href="#">메뉴2</a>
+            </li>
+            <li>
+              <a href="#">메뉴3</a>
+            </li>
+            <li>
+              <a href="#">메뉴4</a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.header_right}>
+          <ul className={styles.header_rightUL}>
+            <li>
+              <a href="#">
+                <img src="../assets/plus.svg" alt="" width={20} height={20} />
+              </a>
+            </li>
+            <li>
+              <a href="#">ko</a>
+            </li>
+            <li>
+              <a href="#">로그인</a>
+            </li>
+            <li>
+              <a href="#">회원가입</a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../assets/search.svg" alt="" width={30} height={30} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Header from "../components/Header.jsx/Header";
+import Body from "../components/Body/body";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
+
+export default App;
